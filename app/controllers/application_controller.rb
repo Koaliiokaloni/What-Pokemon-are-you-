a2da31e66444d4e6db9fda1dd3220a56fa8cfc46
Andrew StoneAwong <p1s2h3d4a5v6v@gmail.com>
2016-07-22 19:00:37 +0000

    need to update results

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,17 +8,56 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-
+  
   get '/' do 
     erb :index
   end
 
-get '/questions' do
-  erb :questions
+get '/q1' do
+  erb :q1
 end
 
-  post '/questions' do
-    erb :questions
+  post '/q1' do
+    erb :q1
   end
 
+get '/q2' do
+  erb :q2
+end
+
+  post '/q2' do
+    erb :q2
+  end
+  
+  get '/q3' do
+  erb :q3
+end
+
+  post '/q3' do
+    erb :q3
+  end
+  
+  get '/q4' do
+  erb :q4
+end
+
+  post '/q4' do
+    erb :q4
+  end
+  
+  get '/q5' do
+  erb :q5
+end
+
+  post '/q5' do
+    erb :q5
+  end
+  
+  get '/Results' do
+  erb :Results
+end
+
+  post '/Results' do
+    erb :Results
+  end
 end
